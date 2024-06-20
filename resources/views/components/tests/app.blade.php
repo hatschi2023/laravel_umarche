b47e27bbacf61f43ece9bdce484963fa11aea3c9
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-06-20 13:46:36 +0900

    名前付きスロット

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -18,6 +18,10 @@
 
     </head>
     <body>
+        <header>
+            {{ $header }}
+        </header>
+
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,6 @@
 
 <x-tests.app>
+    <x-slot name="header">ヘッダー2</x-slot>
+
     コンポーネント2
 </x-tests.app>
