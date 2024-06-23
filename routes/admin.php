c40e997c14a1f8c155987b2d01e1114ae2cd5600
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-06-23 23:49:12 +0900

    リソースコントローラ

--- routes/admin.php
+++ routes/admin.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Admin\Auth\NewPasswordController;
 use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
+use App\Http\Controllers\Admin\OwnersController;
+use App\Models\Owner;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,11 +27,12 @@ Route::get('/', function () {
     return view('admin.welcome');
 });
 
+Route::resource('owners', OwnersController::class)
+->middleware('auth:admin');
+
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->middleware(['auth:admin'])->name('dashboard');
-
-
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
