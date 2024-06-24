2bb668c5c68b5e0f2597b5c17763bd359eb26afe
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-06-24 16:09:52 +0900

    index, create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -23,8 +23,8 @@ class OwnersController extends Controller
         // echo $date_now->year;
         // echo $date_parse;
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
@@ -33,8 +33,10 @@ class OwnersController extends Controller
 
         // dd($e_all, $q_get, $q_first, $c_test);
         // dd($q_get);
+
+        $owners = Owner::select('name', 'email', 'created_at')->get();
         return view('admin.owners.index',
-        compact('e_all', 'q_get'));
+        compact('owners'));
     }
 
     /**
@@ -44,7 +46,7 @@ class OwnersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
